8815e186b3bd24ee8dbe429ca978e536b8f5aa56
CedricLismonde <cedric.lismonde@fer.hr>
2025-01-30 14:50:21 +0100

    tour eiffel premiere version

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function afficherMessage() {
-    alert("Bonjour ! Vous avez cliqué sur le bouton.");
+function afficherDetails() {
+    let details = document.getElementById("details");
+    
+    if (details.classList.contains("hidden")) {
+        details.classList.remove("hidden");
+    } else {
+        details.classList.add("hidden");
+    }
 }
